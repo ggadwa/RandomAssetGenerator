dc53326e4c3d6e9f52a2efa662c30ea2a09d2a2a
Brian Barnes <ggadwa@gmail.com>
2022-05-03 02:05:28 -0400

    Model Update, cont

--- src/main/java/com/klinksoftware/rag/mesh/MeshModelUtility.java
+++ src/main/java/com/klinksoftware/rag/mesh/MeshModelUtility.java
@@ -15,17 +15,16 @@ public class MeshModelUtility
         //
         // build a cylinder around a limb
         //
-    public static Mesh buildCylinderAroundLimb(String name, String bitmapName, int meshType, int axis, RagPoint meshScale, RagPoint topPnt, float topRadius, RagPoint botPnt, float botRadius) {
-        int n, k, iIdx, vStartIdx, vIdx;
-        float ang, ang2, angAdd, acrossTop, acrossBot, acrossAdd;
-        float u1, u2, v1, v2;
-        float tx, ty, tz, tx2, ty2, tz2, bx, by, bz, bx2, by2, bz2;
-        float rd, rd2, rAdd, botRad, topRad;
+    public static Mesh buildCylinderAroundLimb(String name, String bitmapName, int meshType, int axis, RagPoint meshScale, RagPoint botPnt, float botRadius, RagPoint topPnt, float topRadius) {
+        int n, k, rowIdx, row2Idx, rowStartIdx, row2StartIdx, vIdx, vStartIdx, vOrigStartIdx;
+        float ang, angAdd, acrossPos, acrossAdd;
+        float tx, ty, tz;
+        float rd, rAdd, rad;
         ArrayList<Float> vertexArray, normalArray, uvArray;
         ArrayList<Integer> indexArray;
         int[] indexes;
         float[] vertexes, normals, tangents, uvs;
-        RagPoint normal, centerPntTop, centerPntBot;
+        RagPoint normal, centerPnt;
 
         // allocate arrays
         vertexArray = new ArrayList<>();
@@ -34,8 +33,7 @@ public class MeshModelUtility
         indexArray = new ArrayList<>();
 
         normal = new RagPoint(0.0f, 0.0f, 0.0f);
-        centerPntTop = new RagPoint(0.0f, 0.0f, 0.0f);
-        centerPntBot = new RagPoint(0.0f, 0.0f, 0.0f);
+        centerPnt = new RagPoint(0.0f, 0.0f, 0.0f);
 
         // cylinder setup
         angAdd = 360.0f / (float) CYLINDER_AROUND_SURFACE_COUNT;
@@ -43,163 +41,104 @@ public class MeshModelUtility
         switch (axis) {
             case Limb.LIMB_AXIS_X:
                 acrossAdd = (topPnt.x - botPnt.x) / (float) CYLINDER_ACROSS_SURFACE_COUNT;
-                acrossBot = botPnt.x;
-                acrossTop = botPnt.x + acrossAdd;
+                acrossPos = botPnt.x;
                 break;
             case Limb.LIMB_AXIS_Y:
                 acrossAdd = (topPnt.y - botPnt.y) / (float) CYLINDER_ACROSS_SURFACE_COUNT;
-                acrossBot = botPnt.y;
-                acrossTop = botPnt.y + acrossAdd;
+                acrossPos = botPnt.y;
                 break;
             default: // Z
                 acrossAdd = (topPnt.z - botPnt.z) / (float) CYLINDER_ACROSS_SURFACE_COUNT;
-                acrossBot = botPnt.z;
-                acrossTop = botPnt.z + acrossAdd;
+                acrossPos = botPnt.z;
                 break;
         }
 
+        rad = botRadius;
         rAdd = (topRadius - botRadius) / (float) CYLINDER_ACROSS_SURFACE_COUNT;
-        botRad = botRadius;
-        topRad = botRad + rAdd;
 
-        // make the cylinder
-        iIdx = 0;
-
-        for (k = 0; k != CYLINDER_ACROSS_SURFACE_COUNT; k++) {
+        // the cylinder vertexes
+        for (k = 0; k != (CYLINDER_ACROSS_SURFACE_COUNT + 1); k++) {
 
             // move center point along line
-            centerPntBot.x = botPnt.x + (((topPnt.x - botPnt.x) * (float) k) / (float) CYLINDER_ACROSS_SURFACE_COUNT);
-            centerPntBot.y = botPnt.y + (((topPnt.y - botPnt.y) * (float) k) / (float) CYLINDER_ACROSS_SURFACE_COUNT);
-            centerPntBot.z = botPnt.z + (((topPnt.z - botPnt.z) * (float) k) / (float) CYLINDER_ACROSS_SURFACE_COUNT);
+            centerPnt.x = botPnt.x + (((topPnt.x - botPnt.x) * (float) k) / (float) CYLINDER_ACROSS_SURFACE_COUNT);
+            centerPnt.y = botPnt.y + (((topPnt.y - botPnt.y) * (float) k) / (float) CYLINDER_ACROSS_SURFACE_COUNT);
+            centerPnt.z = botPnt.z + (((topPnt.z - botPnt.z) * (float) k) / (float) CYLINDER_ACROSS_SURFACE_COUNT);
 
-            centerPntTop.x = botPnt.x + (((topPnt.x - botPnt.x) * (float) (k + 1)) / (float) CYLINDER_ACROSS_SURFACE_COUNT);
-            centerPntTop.y = botPnt.y + (((topPnt.y - botPnt.y) * (float) (k + 1)) / (float) CYLINDER_ACROSS_SURFACE_COUNT);
-            centerPntTop.z = botPnt.z + (((topPnt.z - botPnt.z) * (float) (k + 1)) / (float) CYLINDER_ACROSS_SURFACE_COUNT);
-
-            // the vs
-            v1 = (float) k / (float) CYLINDER_ACROSS_SURFACE_COUNT;
-            v2 = (k == (CYLINDER_ACROSS_SURFACE_COUNT - 1)) ? 1.0f : ((float) (k + 1) / (float) CYLINDER_ACROSS_SURFACE_COUNT);
-
-            // cyliner faces
             ang = 0.0f;
 
             for (n = 0; n != CYLINDER_AROUND_SURFACE_COUNT; n++) {
-                ang2 = ang + angAdd;
-
-                // the two Us
-                u1 = ang / 360.0f;
-                u2 = ang2 / 360.0f;
 
                 // force last segment to wrap
                 if (n == (CYLINDER_AROUND_SURFACE_COUNT - 1)) {
-                    ang2 = 0.0f;
+                    ang = 0.0f;
                 }
 
                 rd = ang * ((float) Math.PI / 180.0f);
-                rd2 = ang2 * ((float) Math.PI / 180.0f);
 
                 switch (axis) {
                     case Limb.LIMB_AXIS_X:
-                        tx = acrossTop;
-                        ty = centerPntTop.y + (((topRad * (float) Math.sin(rd)) + (topRad * (float) Math.cos(rd))) * meshScale.y);
-                        tz = centerPntTop.z + (((topRad * (float) Math.cos(rd)) - (topRad * (float) Math.sin(rd))) * meshScale.z);
-                        bx = acrossBot;
-                        by = centerPntBot.y + (((botRad * (float) Math.sin(rd)) + (botRad * (float) Math.cos(rd))) * meshScale.y);
-                        bz = centerPntBot.z + (((botRad * (float) Math.cos(rd)) - (botRad * (float) Math.sin(rd))) * meshScale.z);
-                        tx2 = acrossTop;
-                        ty2 = centerPntTop.y + (((topRad * (float) Math.sin(rd2)) + (topRad * (float) Math.cos(rd2))) * meshScale.y);
-                        tz2 = centerPntTop.z + (((topRad * (float) Math.cos(rd2)) - (topRad * (float) Math.sin(rd2))) * meshScale.z);
-                        bx2 = acrossBot;
-                        by2 = centerPntBot.y + (((botRad * (float) Math.sin(rd2)) + (botRad * (float) Math.cos(rd2))) * meshScale.y);
-                        bz2 = centerPntBot.z + (((botRad * (float) Math.cos(rd2)) - (botRad * (float) Math.sin(rd2))) * meshScale.z);
+                        tx = acrossPos;
+                        ty = centerPnt.y + (((rad * (float) Math.sin(rd)) + (rad * (float) Math.cos(rd))) * meshScale.y);
+                        tz = centerPnt.z + (((rad * (float) Math.cos(rd)) - (rad * (float) Math.sin(rd))) * meshScale.z);
                         break;
                     case Limb.LIMB_AXIS_Y:
-                        tx = centerPntTop.x + (((topRad * (float) Math.sin(rd)) + (topRad * (float) Math.cos(rd))) * meshScale.x);
-                        ty = acrossTop;
-                        tz = centerPntTop.z + (((topRad * (float) Math.cos(rd)) - (topRad * (float) Math.sin(rd))) * meshScale.z);
-                        bx = centerPntBot.x + (((botRad * (float) Math.sin(rd)) + (botRad * (float) Math.cos(rd))) * meshScale.x);
-                        by = acrossBot;
-                        bz = centerPntBot.z + (((botRad * (float) Math.cos(rd)) - (botRad * (float) Math.sin(rd))) * meshScale.z);
-                        tx2 = centerPntTop.x + (((topRad * (float) Math.sin(rd2)) + (topRad * (float) Math.cos(rd2))) * meshScale.x);
-                        ty2 = acrossTop;
-                        tz2 = centerPntTop.z + (((topRad * (float) Math.cos(rd2)) - (topRad * (float) Math.sin(rd2))) * meshScale.z);
-                        bx2 = centerPntBot.x + (((botRad * (float) Math.sin(rd2)) + (botRad * (float) Math.cos(rd2))) * meshScale.x);
-                        by2 = acrossBot;
-                        bz2 = centerPntBot.z + (((botRad * (float) Math.cos(rd2)) - (botRad * (float) Math.sin(rd2))) * meshScale.z);
+                        tx = centerPnt.x + (((rad * (float) Math.sin(rd)) + (rad * (float) Math.cos(rd))) * meshScale.x);
+                        ty = acrossPos;
+                        tz = centerPnt.z + (((rad * (float) Math.cos(rd)) - (rad * (float) Math.sin(rd))) * meshScale.z);
                         break;
                     default: // Z
-                        tx = centerPntTop.x + (((topRad * (float) Math.sin(rd)) + (topRad * (float) Math.cos(rd))) * meshScale.x);
-                        ty = centerPntTop.y + (((topRad * (float) Math.cos(rd)) - (topRad * (float) Math.sin(rd))) * meshScale.y);
-                        tz = acrossTop;
-                        bx = centerPntBot.x + (((botRad * (float) Math.sin(rd)) + (botRad * (float) Math.cos(rd))) * meshScale.x);
-                        by = centerPntBot.y + (((botRad * (float) Math.cos(rd)) - (botRad * (float) Math.sin(rd))) * meshScale.y);
-                        bz = acrossBot;
-                        tx2 = centerPntTop.x + (((topRad * (float) Math.sin(rd2)) + (topRad * (float) Math.cos(rd2))) * meshScale.x);
-                        ty2 = centerPntTop.y + (((topRad * (float) Math.cos(rd2)) - (topRad * (float) Math.sin(rd2))) * meshScale.y);
-                        tz2 = acrossTop;
-                        bx2 = centerPntBot.x + (((botRad * (float) Math.sin(rd2)) + (botRad * (float) Math.cos(rd2))) * meshScale.x);
-                        by2 = centerPntBot.y + (((botRad * (float) Math.cos(rd2)) - (botRad * (float) Math.sin(rd2))) * meshScale.y);
-                        bz2 = acrossBot;
+                        tx = centerPnt.x + (((rad * (float) Math.sin(rd)) + (rad * (float) Math.cos(rd))) * meshScale.x);
+                        ty = centerPnt.y + (((rad * (float) Math.cos(rd)) - (rad * (float) Math.sin(rd))) * meshScale.y);
+                        tz = acrossPos;
                         break;
                 }
 
-                // the points
                 vertexArray.addAll(Arrays.asList(tx, ty, tz));
-                normal.setFromValues((tx - centerPntTop.x), (ty - centerPntTop.y), (tz - centerPntTop.z));
+                normal.setFromValues((tx - centerPnt.x), (ty - centerPnt.y), (tz - centerPnt.z));
                 normal.normalize();
                 normalArray.addAll(Arrays.asList(normal.x, normal.y, normal.z));
-                uvArray.addAll(Arrays.asList(u1, v1));
-                indexArray.add(iIdx++);
+                uvArray.addAll(Arrays.asList((ang / 360.0f), ((float) k / (float) CYLINDER_ACROSS_SURFACE_COUNT)));
 
-                vertexArray.addAll(Arrays.asList(tx2, ty2, tz2));
-                normal.setFromValues((tx2 - centerPntTop.x), (ty2 - centerPntTop.y), (tz2 - centerPntTop.z));
-                normal.normalize();
-                normalArray.addAll(Arrays.asList(normal.x, normal.y, normal.z));
-                uvArray.addAll(Arrays.asList(u2, v1));
-                indexArray.add(iIdx++);
-
-                vertexArray.addAll(Arrays.asList(bx, by, bz));
-                normal.setFromValues((bx - centerPntBot.x), (by - centerPntBot.y), (bz - centerPntBot.z));
-                normal.normalize();
-                normalArray.addAll(Arrays.asList(normal.x, normal.y, normal.z));
-                uvArray.addAll(Arrays.asList(u1, v2));
-                indexArray.add(iIdx++);
-
-                vertexArray.addAll(Arrays.asList(tx2, ty2, tz2));
-                normal.setFromValues((tx2 - centerPntTop.x), (ty2 - centerPntTop.y), (tz2 - centerPntTop.z));
-                normal.normalize();
-                normalArray.addAll(Arrays.asList(normal.x, normal.y, normal.z));
-                uvArray.addAll(Arrays.asList(u2, v1));
-                indexArray.add(iIdx++);
-
-                vertexArray.addAll(Arrays.asList(bx2, by2, bz2));
-                normal.setFromValues((bx2 - centerPntBot.x), (by2 - centerPntBot.y), (bz2 - centerPntBot.z));
-                normal.normalize();
-                normalArray.addAll(Arrays.asList(normal.x, normal.y, normal.z));
-                uvArray.addAll(Arrays.asList(u2, v2));
-                indexArray.add(iIdx++);
-
-                vertexArray.addAll(Arrays.asList(bx, by, bz));
-                normal.setFromValues((bx - centerPntBot.x), (by - centerPntBot.y), (bz - centerPntBot.z));
-                normal.normalize();
-                normalArray.addAll(Arrays.asList(normal.x, normal.y, normal.z));
-                uvArray.addAll(Arrays.asList(u1, v2));
-                indexArray.add(iIdx++);
-
-                ang = ang2;
+                ang += angAdd;
             }
 
-            botRad = topRad;
-            topRad = botRad + rAdd;
+            rad += rAdd;
+            acrossPos += acrossAdd;
+        }
 
-            acrossBot = acrossTop;
-            acrossTop = acrossBot + acrossAdd;
+        // the cylinder triangles
+        for (k = 0; k != CYLINDER_ACROSS_SURFACE_COUNT; k++) {
+
+            rowIdx = rowStartIdx = k * CYLINDER_AROUND_SURFACE_COUNT;
+            row2Idx = row2StartIdx = rowIdx + CYLINDER_AROUND_SURFACE_COUNT;
+
+            for (n = 0; n != CYLINDER_AROUND_SURFACE_COUNT; n++) {
+
+                if (n == (CYLINDER_AROUND_SURFACE_COUNT - 1)) {
+                    indexArray.add(rowIdx);
+                    indexArray.add(rowStartIdx);
+                    indexArray.add(row2Idx);
+                    indexArray.add(rowStartIdx);
+                    indexArray.add(row2StartIdx);
+                    indexArray.add(row2Idx);
+                } else {
+                    indexArray.add(rowIdx);
+                    indexArray.add(rowIdx + 1);
+                    indexArray.add(row2Idx);
+                    indexArray.add(rowIdx + 1);
+                    indexArray.add(row2Idx + 1);
+                    indexArray.add(row2Idx);
+                }
+
+                rowIdx++;
+                row2Idx++;
+            }
         }
 
         // top close
         if ((meshType == Limb.MESH_TYPE_CYLINDER_CLOSE_ALL) || (meshType == Limb.MESH_TYPE_CYLINDER_CLOSE_TOP)) {
             vIdx = vertexArray.size() / 3;
-            vStartIdx = vIdx - (CYLINDER_AROUND_SURFACE_COUNT * 6);
+            vStartIdx = vOrigStartIdx = CYLINDER_AROUND_SURFACE_COUNT * (CYLINDER_ACROSS_SURFACE_COUNT + 0);
 
             // middle vertex
             vertexArray.addAll(Arrays.asList(topPnt.x, topPnt.y, topPnt.z));
@@ -208,104 +147,33 @@ public class MeshModelUtility
             normalArray.addAll(Arrays.asList(normal.x, normal.y, normal.z));
             uvArray.addAll(Arrays.asList(0.5f, 0.5f));
 
-            for (n = 0; n != (CYLINDER_AROUND_SURFACE_COUNT - 2); n++) {
+            for (n = 0; n != (CYLINDER_AROUND_SURFACE_COUNT - 1); n++) {
                 indexArray.add(vStartIdx);
-                indexArray.add(vStartIdx + 1);
+                indexArray.add((n == (CYLINDER_AROUND_SURFACE_COUNT - 2)) ? vOrigStartIdx : (vStartIdx + 1));
                 indexArray.add(vIdx);
 
-                vStartIdx += 6;   // there's 6 vertexes to each two points on the edge
+                vStartIdx++;
             }
-
-            /*
-            vStartIdx = vertexArray.size() / 3;
-
-            ang = 0.0f;
-
-            for (n = 0; n != CYLINDER_AROUND_SURFACE_COUNT; n++) {
-                rd = ang * ((float) Math.PI / 180.0f);
-
-                u1 = ((float) Math.sin(rd) * 0.5f) + 0.5f;
-                u2 = ((float) Math.cos(rd) * 0.5f) + 0.5f;
-
-                switch (axis) {
-                    case Limb.LIMB_AXIS_X:
-                        tx = topPnt.x;
-                        ty = topPnt.x + ((topRad * (float) Math.sin(rd)) + (topRad * (float) Math.cos(rd)));
-                        tz = topPnt.z + ((topRad * (float) Math.cos(rd)) - (topRad * (float) Math.sin(rd)));
-                    case Limb.LIMB_AXIS_Y:
-                        tx = topPnt.x + ((topRad * (float) Math.sin(rd)) + (topRad * (float) Math.cos(rd)));
-                        ty = topPnt.y;
-                        tz = topPnt.z + ((topRad * (float) Math.cos(rd)) - (topRad * (float) Math.sin(rd)));
-                        break;
-                    default: // Z
-                        tx = topPnt.x + ((topRad * (float) Math.sin(rd)) + (topRad * (float) Math.cos(rd)));
-                        ty = topPnt.z + ((topRad * (float) Math.cos(rd)) - (topRad * (float) Math.sin(rd)));
-                        tz = topPnt.z;
-                        break;
-                }
-
-                // the points
-                vertexArray.addAll(Arrays.asList(tx, ty, tz));
-                normal.setFromValues((tx - topPnt.x), (ty - topPnt.y), (tz - topPnt.z));
-                normal.normalize();
-                normalArray.addAll(Arrays.asList(normal.x, normal.y, normal.z));
-                uvArray.addAll(Arrays.asList(u1, u2));
-
-                ang += angAdd;
-            }
-
-            for (n = 0; n != (CYLINDER_AROUND_SURFACE_COUNT - 2); n++) {
-                indexArray.add(vStartIdx);
-                indexArray.add(vStartIdx + (n + 1));
-                indexArray.add(vStartIdx + (n + 2));
-            }
-             */
         }
 
         if ((meshType == Limb.MESH_TYPE_CYLINDER_CLOSE_ALL) || (meshType == Limb.MESH_TYPE_CYLINDER_CLOSE_BOTTOM)) {
-            /*
-            vStartIdx = vertexArray.size() / 3;
+            vIdx = vertexArray.size() / 3;
+            vStartIdx = 0;
 
-            ang = 0.0f;
+            // middle vertex
+            vertexArray.addAll(Arrays.asList(botPnt.x, botPnt.y, botPnt.z));
+            normal.setFromValues((botPnt.x - topPnt.x), (botPnt.y - topPnt.y), (botPnt.z - topPnt.z));
+            normal.normalize();
+            normalArray.addAll(Arrays.asList(normal.x, normal.y, normal.z));
+            uvArray.addAll(Arrays.asList(0.5f, 0.5f));
 
-            for (n = 0; n != CYLINDER_AROUND_SURFACE_COUNT; n++) {
-                rd = ang * ((float) Math.PI / 180.0f);
-
-                u1 = ((float) Math.sin(rd) * 0.5f) + 0.5f;
-                u2 = ((float) Math.cos(rd) * 0.5f) + 0.5f;
-
-                switch (axis) {
-                    case Limb.LIMB_AXIS_X:
-                        bx = botPnt.x;
-                        by = botPnt.x + ((botRad * (float) Math.sin(rd)) + (botRad * (float) Math.cos(rd)));
-                        bz = botPnt.z + ((botRad * (float) Math.cos(rd)) - (botRad * (float) Math.sin(rd)));
-                    case Limb.LIMB_AXIS_Y:
-                        bx = botPnt.x + ((botRad * (float) Math.sin(rd)) + (botRad * (float) Math.cos(rd)));
-                        by = botPnt.y;
-                        bz = botPnt.z + ((botRad * (float) Math.cos(rd)) - (botRad * (float) Math.sin(rd)));
-                    default: // Z
-                        bx = botPnt.x + ((botRad * (float) Math.sin(rd)) + (botRad * (float) Math.cos(rd)));
-                        by = botPnt.z + ((botRad * (float) Math.cos(rd)) - (botRad * (float) Math.sin(rd)));
-                        bz = botPnt.z;
-                        break;
-                }
-                // the points
-
-                vertexArray.addAll(Arrays.asList(bx, by, bz));
-                normal.setFromValues((bx - botPnt.x), (by - botPnt.y), (bz - botPnt.z));
-                normal.normalize();
-                normalArray.addAll(Arrays.asList(normal.x, normal.y, normal.z));
-                uvArray.addAll(Arrays.asList(u1, u2));
-
-                ang += angAdd;
-            }
-
-            for (n = 0; n != (CYLINDER_AROUND_SURFACE_COUNT - 2); n++) {
+            for (n = 0; n != (CYLINDER_AROUND_SURFACE_COUNT - 1); n++) {
                 indexArray.add(vStartIdx);
-                indexArray.add(vStartIdx + (n + 1));
-                indexArray.add(vStartIdx + (n + 2));
+                indexArray.add((n == (CYLINDER_AROUND_SURFACE_COUNT - 2)) ? 0 : (vStartIdx + 1));
+                indexArray.add(vIdx);
+
+                vStartIdx++;
             }
-             */
         }
 
         // create the mesh
